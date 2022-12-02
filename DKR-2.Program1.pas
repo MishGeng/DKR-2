74b925511fba62bd0ef4993bc62d7af6bba49941
MishGeng <117731137+MishGeng@users.noreply.github.com>
2022-12-02 10:50:22 +0300

    Add files via upload

---
+++ DKR-2.Program1.pas
@@ -0,0 +1,20 @@
+﻿var
+  a: array[0..9999] of integer;
+begin
+  var n := ReadInteger('Введите размер массива');
+  println('Данн масив');
+  for var i := 0 to n do
+  begin
+    a[i] := random(0, 100);
+    print(a[i])
+  end;
+  println;
+  var m := ReadInteger('Введите число');
+  for var i := 0 to n do
+    if a[i] = m then
+    begin
+      Writeln('В этом массиве есть заданный элемент');
+      exit();
+    end;
+  Writeln('В этом массиве нет заданного элемента')
+end.
---
+++ DKR-2.Program2.pas
@@ -0,0 +1,23 @@
+﻿begin
+  var z,slovo,b:integer;
+  var s:= readstring('введите строку');
+  z:=length(s);
+    for var i:=1 to z do 
+    begin    
+    if s[i] = ' ' then
+      inc(slovo);
+    case s[i] of
+      'a'..'z': inc(b);
+      'A'..'Z': inc(b);
+      'а'..'я': inc(b);
+      'А'..'Я': inc(b);
+      end;
+    if (s[i]>='0') and (s[i]<='9')
+    then begin 
+    print('это не слово'); 
+    exit;
+    end;
+    end;
+    print('буквы:',b);
+    print('cлова:',slovo+1);
+  end.
